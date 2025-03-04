bb2a25369bd7fa9fcd6ddabfcb435f0c4cb84d52
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-03-04 23:49:23 +0100

    Create add new task

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/app/todo_app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const TodoApp());
+  runApp(
+    const ProviderScope(
+      child: TodoApp(),
+      ),
+    );
 }
 
 
