8c09599cbc7c3912e907edef569c6a952577242c
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-03-06 19:36:24 +0100

    Datasource

--- lib/data/models/task.dart
+++ lib/data/models/task.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:todoapp/utils/task_categories.dart';
+import '../../utils/utils.dart';
 
 class Task extends Equatable {
   final int? id;
@@ -32,5 +33,29 @@ class Task extends Equatable {
       date,
       isCompleted,
     ];
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      TaskKeys.id: id,
+      TaskKeys.title: title,
+      TaskKeys.note: note,
+      TaskKeys.time: time,
+      TaskKeys.date: date,
+      TaskKeys.category: category.name,
+      TaskKeys.isCompleted: isCompleted,
+    };
+  }
+
+  factory Task.fromJson(Map<String, dynamic> map) {
+    return Task(
+      id: map[TaskKeys.id],
+      title: map[TaskKeys.title],
+      note: map[TaskKeys.note],
+      time: map[TaskKeys.time],
+      date: map[TaskKeys.date],
+      category: TaskCategories.stringToCategory(map[TaskKeys.category]),
+      isCompleted: map['isCompleted'],
+    );
   }
 }
