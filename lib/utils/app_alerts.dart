649ee4ad035b5ecddb6e5c6b6fc1715587326395
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-03-09 18:56:55 +0100

    Finish the app

--- lib/utils/app_alerts.dart
+++ lib/utils/app_alerts.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:todoapp/data/models/task.dart';
+import 'package:todoapp/providers/task/task.dart';
 import 'package:todoapp/utils/utils.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class AppAlerts {
   AppAlerts._();
@@ -16,6 +20,39 @@ class AppAlerts {
         ),
         backgroundColor: context.colorScheme.primary,
       ),
+    );
+  }
+
+  static Future<void> showDeleteAlertDialog(BuildContext context, WidgetRef ref, Task task) async {
+    Widget cancelButton = TextButton(
+      onPressed: () => context.pop(), 
+      child: const Text('No'),
+    );
+    Widget deleteButton = TextButton(
+      onPressed: () async {
+        await ref.read(taskProvider.notifier).deleteTask(task).then((value) {
+          displaySnackbar(
+            context, 
+            'Task delete successfully',
+          );
+          context.pop();
+        });
+      }, 
+      child: const Text('Yes'),
+    );
+    AlertDialog alert = AlertDialog(
+      title: const Text('Are you sure you want to delete this task?'),
+      actions: [
+        deleteButton,
+        cancelButton,
+      ],
+    );
+
+    await showDialog(
+      context: context, 
+      builder: (context) {
+        return alert;
+      },
     );
   }
 }
